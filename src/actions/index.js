9ebc79346dda5ccab8bca4032868a4a54bd35fdc
Henry Yang <yzh813@gmail.com>
2018-09-17 22:19:02 -0700

    weather reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,7 +7,9 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
 	const url = `${ROOT_URL}&q=${city},us`;
-	const request = axios.get(url);
+	const request = axios.get(url); //promise
+
+	console.log('Request', request);
 
 	return {
 		type: FETCH_WEATHER,
